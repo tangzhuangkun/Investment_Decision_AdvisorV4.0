00dff92f14aa4a7ee057bfa6ff1bea429343b758
AdhocSun <1029791755@qq.com>
2022-03-25 20:21:42 +0800

    解决 从腾讯接口 获取指数/股票最新的涨跌率 报错问题

--- data_collector/data_collector_common_index_collector.py
+++ data_collector/data_collector_common_index_collector.py
@@ -12,6 +12,8 @@ class DataCollectorCommonIndexCollector:
     def __init__(self):
         pass
 
+
+    """
     def get_target_latest_increasement_decreasement_rate(self, code_with_location):
         '''
         获取指数/股票最新的涨跌率
@@ -27,9 +29,30 @@ class DataCollectorCommonIndexCollector:
         # 接口返回如： 涨跌率, -0.86;
         # 只取 涨跌率
         url = 'https://qt.gtimg.cn/q=s_'+code_with_location
-        content = requests.get(url, headers=header, proxies=proxy)
+        content = requests.get(url, headers=header, proxies=proxy, verify=False)
         content_split = content.text.split('~')
         return decimal.Decimal(content_split[5])
+    """
+
+    def get_target_latest_increasement_decreasement_rate(self, code_with_location):
+        '''
+        获取指数/股票最新的涨跌率
+        :param code_with_location: 指数/股票代码(含上市地), 必须如 sz399965，代码前面带上市地
+        :return: 最新的涨跌幅, 如 0.39% 即返回为 0.39
+        '''
+
+        # 接口地址
+        # 接口返回: 指数名称，当前点数，当前价格，涨跌, 涨跌率，成交量（手），成交额（万元）, 总市值；
+        # 接口返回如： 涨跌率, -0.86;
+        # 只取 涨跌率
+        url = 'https://qt.gtimg.cn/q=s_' + code_with_location
+        content = requests.get(url, verify=False)
+        content_split = content.text.split('~')
+        return decimal.Decimal(content_split[5])
+
+
+
+
 
 if __name__ == '__main__':
     time_start = time.time()
--- data_collector/get_stock_real_time_indicator_from_interfaces.py
+++ data_collector/get_stock_real_time_indicator_from_interfaces.py
@@ -28,6 +28,82 @@ class GetStockRealTimeIndicatorFromInterfaces:
     # http: // quote.eastmoney.com / sh600519.html
     # A股股票ROE
 
+    def get_single_stock_real_time_indicator(self, stock_id, indicator):
+        '''
+        解析接口信息,从接口获取实时的股票指标
+        :param stock_id: 股票代码（2位上市地+6位数字， 如 sz000596）
+        :param indicator: 需要抓取的指标，如 pe_ttm,市盈率TTM；pb,市净率，dr_ttm,滚动股息率 等
+        :return: 获取的实时的股票滚动市盈率 格式如 32.74
+        '''
+
+        # 地址模板
+        interface_address = 'https://qt.gtimg.cn/q=' + stock_id
+
+        # 递归算法，处理异常
+        try:
+            # 增加连接重试次数,默认10次
+            requests.adapters.DEFAULT_RETRIES = 10
+            # 关闭多余的连接：requests使用了urllib3库，默认的http connection是keep-alive的，
+            # requests设置False关闭
+            s = requests.session()
+            s.keep_alive = False
+
+            # 忽略警告
+            requests.packages.urllib3.disable_warnings()
+
+            # 得到接口返回的信息
+            raw_data = requests.get(interface_address,  verify=False, stream=False,
+                                    timeout=10).text
+
+            data_list = raw_data.split("~")
+            if indicator == "pe_ttm":
+                return data_list[39]
+            elif indicator == "pb":
+                return data_list[46]
+            elif indicator == "dr_ttm":
+                return data_list[64]
+            else:
+                # 日志记录
+                msg = "Unknown indicator"
+                custom_logger.CustomLogger().log_writter(msg, lev='warning')
+                # 返回 空
+                return -10000
+
+        # 如果读取超时，重新在执行一遍解析页面
+        except requests.exceptions.ReadTimeout:
+            # 日志记录
+            msg = "Collected stock real time " + indicator + " from " + interface_address + '  ' + "ReadTimeout"
+            custom_logger.CustomLogger().log_writter(msg, lev='warning')
+            # 返回解析页面得到的股票指标
+            return self.get_single_stock_real_time_indicator(stock_id, indicator)
+
+        # 如果连接请求超时，重新在执行一遍解析页面
+        except requests.exceptions.ConnectTimeout:
+            # 日志记录
+            msg = "Collected stock real time " + indicator + " from " + interface_address + '  ' + "ConnectTimeout"
+            custom_logger.CustomLogger().log_writter(msg, lev='warning')
+            # 返回解析页面得到的股票指标
+            return self.get_single_stock_real_time_indicator(stock_id, indicator)
+
+        # 如果请求超时，重新在执行一遍解析页面
+        except requests.exceptions.Timeout:
+            # 日志记录
+            msg = "Collected stock real time " + indicator + " from " + interface_address + '  ' + "Timeout"
+            custom_logger.CustomLogger().log_writter(msg, lev='warning')
+            # 返回解析页面得到的股票指标
+            return self.get_single_stock_real_time_indicator(stock_id, indicator)
+
+        except Exception as e:
+            # 日志记录
+            msg = interface_address + str(e)
+            custom_logger.CustomLogger().log_writter(msg, lev='warning')
+            # 返回解析页面得到的股票指标
+            return self.get_single_stock_real_time_indicator(stock_id, indicator)
+
+
+
+    """
+    
     def get_single_stock_real_time_indicator(self, stock_id, header, proxy, indicator):
         '''
         解析接口信息,从接口获取实时的股票指标
@@ -101,6 +177,8 @@ class GetStockRealTimeIndicatorFromInterfaces:
             custom_logger.CustomLogger().log_writter(msg, lev='warning')
             # 返回解析页面得到的股票指标
             return self.get_single_stock_real_time_indicator(stock_id, header, proxy, indicator)
+    
+    """
 
 
     '''
