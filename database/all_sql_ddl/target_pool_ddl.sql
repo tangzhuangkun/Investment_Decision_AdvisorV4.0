ece83a0718cfcbe598353b44f00ea5cd23a41d05
AdhocSun <1029791755@qq.com>
2022-03-30 19:14:14 +0800

    investment_target表，增加 交易方向字段

--- database/all_sql_ddl/target_pool_ddl.sql
+++ database/all_sql_ddl/target_pool_ddl.sql
@@ -11,6 +11,7 @@ CREATE TABLE IF NOT EXISTS `investment_target`(
     `exchange_location`  VARCHAR(10) NOT NULL COMMENT '标的上市地，如 sh,sz,hk',
     `exchange_location_mic`  VARCHAR(10) NOT NULL COMMENT '标的上市地MIC，如 XSHG, XSHE，XHKG 等',
 	`hold_or_not`  tinyint(1) NOT NULL COMMENT '当前是否持有,1为持有，0不持有',
+	`trade` VARCHAR(20) NOT NULL COMMENT '交易方向, 买入-buy, 卖出-sell',
 	`valuation_method` VARCHAR(20) NOT NULL COMMENT '估值方法, pb,pe,ps,dr,roe,peg 等',
 	`trigger_value`  DECIMAL(6,2) NOT NULL COMMENT '估值触发绝对值值临界点，含等于，看指标具体该大于等于还是小于等于，如 pb估值时，0.95',
 	`trigger_percent`  DECIMAL(6,2) NOT NULL COMMENT '估值触发历史百分比临界点，含等于，看指标具体该大于等于还是小于等于，如 10，即10%位置',
@@ -22,42 +23,42 @@ CREATE TABLE IF NOT EXISTS `investment_target`(
 	`p_day` DATE DEFAULT NULL COMMENT '提交的日期',
 	`submission_time` TIMESTAMP DEFAULT CURRENT_TIMESTAMP COMMENT '提交时间',
 	`update_time` TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT '更新时间',
-	UNIQUE INDEX (target_type, target_code, exchange_location, valuation_method, monitoring_frequency, holder),
+	UNIQUE INDEX (target_type, target_code, exchange_location, trade, valuation_method, monitoring_frequency, holder),
 	PRIMARY KEY ( `id` )
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8
 COMMENT '跟踪标的及策略池';
 
 
 insert into investment_target (target_type, target_code, target_name, index_company, exchange_location,
-                               exchange_location_mic, hold_or_not, valuation_method, trigger_value, trigger_percent, monitoring_frequency, holder, status, p_day)
-values ('index', '399997','中证白酒','中证','sz','XSHE', 0, 'pe', 25, 30, 'daily', 'zhuangkun', 'active','2022-03-27');
+                               exchange_location_mic, hold_or_not, trade, valuation_method, trigger_value, trigger_percent, monitoring_frequency, holder, status, p_day)
+values ('index', '399997','中证白酒','中证','sz','XSHE', 0, 'buy','pe', 25, 30, 'daily', 'zhuangkun', 'active','2022-03-27');
 
 insert into investment_target (target_type, target_code, target_name, index_company, exchange_location,
-                               exchange_location_mic, hold_or_not, valuation_method, trigger_value, trigger_percent, monitoring_frequency, holder, status, p_day)
-values ('index', '399396','国证食品饮料行业','国证','sz','XSHE', 0, 'pe', 25, 30, 'daily', 'zhuangkun', 'active', '2022-03-27');
+                               exchange_location_mic, hold_or_not, trade, valuation_method, trigger_value, trigger_percent, monitoring_frequency, holder, status, p_day)
+values ('index', '399396','国证食品饮料行业','国证','sz','XSHE', 0, 'buy', 'pe', 25, 30, 'daily', 'zhuangkun', 'active', '2022-03-27');
 
 insert into investment_target (target_type, target_code, target_name, index_company, exchange_location,
-                               exchange_location_mic, hold_or_not, valuation_method, trigger_value, trigger_percent, monitoring_frequency, holder, status, p_day)
-values ('index', '000932','中证800消费','中证','sz','XSHE', 0, 'pe', 25, 30, 'daily', 'zhuangkun', 'active', '2022-03-27');
+                               exchange_location_mic, hold_or_not, trade, valuation_method, trigger_value, trigger_percent, monitoring_frequency, holder, status, p_day)
+values ('index', '000932','中证800消费','中证','sz','XSHE', 0, 'buy', 'pe', 25, 30, 'daily', 'zhuangkun', 'active', '2022-03-27');
 
 insert into investment_target (target_type, target_code, target_name, index_company, exchange_location,
-                               exchange_location_mic, hold_or_not, valuation_method, trigger_value, trigger_percent, monitoring_frequency, holder, status, p_day)
-values ('index', '399965','中证800地产','中证','sz','XSHE', 0, 'pb', 0.9, 2, 'daily', 'zhuangkun', 'active','2022-03-27');
+                               exchange_location_mic, hold_or_not, trade, valuation_method, trigger_value, trigger_percent, monitoring_frequency, holder, status, p_day)
+values ('index', '399965','中证800地产','中证','sz','XSHE', 0, 'buy', 'pb', 0.9, 2, 'daily', 'zhuangkun', 'active','2022-03-27');
 
 insert into investment_target (target_type, target_code, target_name, index_company, exchange_location,
-                               exchange_location_mic, hold_or_not, valuation_method, trigger_value, trigger_percent, monitoring_frequency, holder, status, p_day)
-values ('index', '399986','中证银行','中证','sz','XSHE', 0, 'pb', 0.9, 2, 'daily', 'zhuangkun', 'active', '2022-03-27');
+                               exchange_location_mic, hold_or_not, trade, valuation_method, trigger_value, trigger_percent, monitoring_frequency, holder, status, p_day)
+values ('index', '399986','中证银行','中证','sz','XSHE', 0, 'buy', 'pb', 0.9, 2, 'daily', 'zhuangkun', 'active', '2022-03-27');
 
 insert into investment_target (target_type, target_code, target_name, index_company, exchange_location,
-                               exchange_location_mic, hold_or_not, valuation_method, trigger_value, trigger_percent, monitoring_frequency, holder, status, p_day)
-values ('index', '000036','中证上证消费','中证','sz','XSHE', 0, 'pe', 25, 30, 'daily', 'zhuangkun', 'active', '2022-03-27');
+                               exchange_location_mic, hold_or_not, trade, valuation_method, trigger_value, trigger_percent, monitoring_frequency, holder, status, p_day)
+values ('index', '000036','中证上证消费','中证','sz','XSHE', 0, 'buy', 'pe', 25, 30, 'daily', 'zhuangkun', 'active', '2022-03-27');
 
 
 
 insert into investment_target (target_type, target_code, target_name, exchange_location,
-                               exchange_location_mic, hold_or_not, valuation_method, trigger_value, trigger_percent, monitoring_frequency, holder, status, p_day)
-values ('stock','000002','万科A','sz','XSHE',0,'pb',0.9,5,'minutely', 'zhuangkun', 'active','2022-03-27');
+                               exchange_location_mic, hold_or_not, trade, valuation_method, trigger_value, trigger_percent, monitoring_frequency, holder, status, p_day)
+values ('stock','000002','万科A','sz','XSHE',0, 'buy', 'pb',0.9,5,'minutely', 'zhuangkun', 'active','2022-03-27');
 
 insert into investment_target (target_type, target_code, target_name, exchange_location,
-                               exchange_location_mic, hold_or_not, valuation_method, trigger_value, trigger_percent, monitoring_frequency, holder, status, p_day)
-values ('stock','600048','保利发展','sh','XSHG',0,'pb',0.89,20,'minutely', 'zhuangkun', 'active','2022-03-27');
+                               exchange_location_mic, hold_or_not, trade, valuation_method, trigger_value, trigger_percent, monitoring_frequency, holder, status, p_day)
+values ('stock','600048','保利发展','sh','XSHG',0, 'buy', 'pb',0.89,20,'minutely', 'zhuangkun', 'active','2022-03-27');
